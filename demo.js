834b46b01e60056586a24b18b6097f3c932da8f3
Rohit Sinha <rohitsinha2705@gmail.com>
2023-02-25 00:00:24 +0530

    JS file

---
+++ demo.js
@@ -0,0 +1,4 @@
+var title = document.getElementById("header-title");
+title.style.borderBottom = 'solid 3px #000';
+var addItem = document.querySelector('.title');
+addItem.style.fontWeight = '900';
