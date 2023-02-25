bbb1194e9b80f2270eed7382f6b917fc0f862b61
Rohit Sinha <rohitsinha2705@gmail.com>
2023-02-26 00:01:24 +0530

    get element by class name

--- demo.js
+++ demo.js
@@ -1,4 +1,12 @@
-var title = document.getElementById("header-title");
-title.style.borderBottom = 'solid 3px #000';
-var addItem = document.querySelector('.title');
-addItem.style.fontWeight = '900';
+//var title = document.getElementById("header-title");
+//title.style.borderBottom = 'solid 3px #000';
+//var addItem = document.querySelector('.title');
+//addItem.style.fontWeight = '900';
+
+// get elements by class
+var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = "green";
+
+for (var i=0; i<items.length; i++){
+    items[i].style.fontWeight ='bold';
+}
