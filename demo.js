e4d59e905516d907bd5294beeb6ea0eec9c2516a
Rohit Sinha <rohitsinha2705@gmail.com>
2023-02-26 23:33:15 +0530

    get elements by TagName

--- demo.js
+++ demo.js
@@ -4,9 +4,22 @@
 //addItem.style.fontWeight = '900';
 
 // get elements by class
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = "green";
+//var items = document.getElementsByClassName('list-group-item');
+//items[2].style.backgroundColor = "green";
 
-for (var i=0; i<items.length; i++){
+/*for (var i=0; i<items.length; i++){
     items[i].style.fontWeight ='bold';
+}*/
+
+// get elements by tagname & classname
+var li = document.getElementsByClassName('list-group-item');
+for(var i=0; i<li.length; i++){
+    li[i].style.fontWeight="bold";
+}
+
+
+
+var li = document.getElementsByTagName('li');
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor="grey";
 }
