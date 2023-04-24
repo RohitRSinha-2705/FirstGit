ac95a333d8b6bbdb1ffdc74a8f51a3e00af6116a
Rohit Sinha <rohitsinha2705@gmail.com>
2023-04-24 23:14:51 +0530

    strore multiple users

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ function addItem(e){
     e.preventDefault();
     var Username = document.getElementById('User-name').value;
     var Email = document.getElementById('E-mail').value;
-    localStorage.setItem('Username',Username);
-    localStorage.setItem('Email',Email);
+    //localStorage.setItem('Username',Username);
+    //localStorage.setItem('Email',Email);
+    const obj = {
+      Username,
+      Email
+    }
+    localStorage.setItem(obj.Username, JSON.stringify(obj));
+    showUser(obj);
   
   }
+function showUser(obj){
+  const parentItem = document.getElementById("listofitems");
+  const childItem = document.createElement("li");
+  childItem.textContent = obj.Username + " - " + obj.Email;
+  parentItem.append(childItem);
+}
